485cd4278d0186ac7ec028a3a0199d25889a9570
Mati Barisone <62723688+MatiBarisone@users.noreply.github.com>
2024-11-16 20:40:45 -0300

    base en atlas prueba

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import { engine } from "express-handlebars"
-import { Server } from "socket.io" 
+import { Server } from "socket.io"
+import mongoose from 'mongoose'
+import cors from 'cors'
 import { router as routerProducts } from "./routes/products.router.js"
 import { router as routerCarts } from "./routes/carts.router.js"
 import { router as routerViews } from "./routes/views.router.js"
 
 const PORT = 8080
 const app = express()
+
+app.use(cors());
 
 //Express Configuration
 app.use(express.json())
@@ -19,9 +23,6 @@ app.use(express.static("./src/public"))
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 app.set("views", "./src/views")
-
-//Websocket Configuration
-
 
 //Routes API
 app.use("/api/products", routerProducts)
@@ -41,3 +42,20 @@ export const io = new Server(server);
 io.on("connection", socket=>{
     console.log(`Se conecto un cliente con ID = ${socket.id}`)
 })
+
+//Mongoose Configuration
+const conectarDB = async() =>{
+    try {
+        await mongoose.connect(
+            'mongodb+srv://MatiasBarisone:jPLUxgJO63aF77zI@cluster0matiasbarisone.3585v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0MatiasBarisone',
+            {
+                dbName: "ecommerceBarisone"
+            }
+        )
+        console.log("Date Base online :)")
+
+    } catch (error) {
+        console.log(`Error en la conexión de la Base de Datos: ${error.message}`)
+    }
+}
+conectarDB()
